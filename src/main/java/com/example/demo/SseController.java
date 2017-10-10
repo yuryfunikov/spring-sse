ab57758fc6da83580faa934c56281d22a35eee13
Brian Clozel <bclozel@pivotal.io>
2017-10-10 09:45:17 +0200

    Test refactoring

--- src/main/java/com/example/demo/SseController.java
+++ src/main/java/com/example/demo/SseController.java
@@ -1,141 +1,64 @@
 package com.example.demo;
 
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-import java.io.IOException;
 
 /**
  * Created by oxygen on 18.08.17.
  */
 @RestController
 public class SseController {
-    private static final Logger logger = LoggerFactory.getLogger(SseController.class);
+	private static final Logger logger = LoggerFactory.getLogger(SseController.class);
 
 
-//    @RequestMapping("/exception")
-//    public ResponseEntity<String> exception() {
-//        throw new RuntimeException("test");
-//    }
+	public AtomicInteger serverCallCount = new AtomicInteger();
 
-    public volatile int callsNo = 0;
+	private AsyncTaskExecutor executor = new SimpleAsyncTaskExecutor("SSE-");
 
-    @RequestMapping("/sse")
-    public SseEmitter sseEmitter(int callNo) {
-        logger.info("/sse/{}", callNo);
-        callsNo ++;
-        SseEmitter emitter = new SseEmitter();
+	@RequestMapping("/sse/{callNumber}")
+	public SseEmitter sseEmitter(@PathVariable int callNumber, HttpServletRequest request) {
+		final int currentCallCount = serverCallCount.incrementAndGet();
+		logger.info("[SERVER] /sse/{}, server call {}, dispatcher {}",
+				callNumber, currentCallCount, request.getDispatcherType().toString());
+		Assert.state(currentCallCount == callNumber,
+				"Server call " + currentCallCount + "; client call " + callNumber +
+						"; dispatcher: " + request.getDispatcherType().toString());
+		final SseEmitter emitter = new SseEmitter();
 
-        new Thread(()->{
-            for (int i = 0; i < 1000; i++) {
-                try {
-                    logger.info("next: {}", i);
-                    emitter.send("next: " + i);
-                } catch (IOException e) {
-                    logger.info("IOException ");
-                    break;
-                }
-                try {
-                    Thread.sleep(1000L);
-                } catch (InterruptedException e) {
-                    logger.info("interrupted...");
-                    break;
-                }
-            }
-            emitter.complete();
-        }).start();
+		executor.execute(() -> {
+			try {
+				for (int i = 0; i < 1000; i++) {
+					logger.info("[SERVER] next: {}", i);
+					emitter.send("next: " + i);
+					Thread.sleep(1000L);
+				}
+				emitter.complete();
+			}
+			catch (IOException e) {
+				logger.info("IOException ");
+				emitter.completeWithError(e);
+			}
+			catch (InterruptedException e) {
+				logger.info("[SERVER] interrupted...");
+				emitter.completeWithError(e);
+			}
+		}, AsyncTaskExecutor.TIMEOUT_IMMEDIATE);
 
-        return emitter;
-
-    }
-
-//    @RequestMapping("/sse2")
-//    public SseEmitter sse2() {
-//
-//        System.err.println("/sse");
-//
-//        SseEmitter emitter = new SseEmitter();
-//
-//
-//        Disposable s = Flux.create(fluxSink -> {
-//
-//            AtomicBoolean cancelled = new AtomicBoolean(false);
-////                fluxSink.onDispose(d->System.err.println("disposed"));
-//            fluxSink
-//                    .onDispose(() -> {
-//                        System.err.println("onCancel");
-//                        cancelled.set(true);
-//                    });
-//
-////            fluxSink.onDipose(() -> cancelled.set(true));
-//
-//            for (int i = 0; i < 10000; i++) {
-//                System.err.println(i);
-//                if (cancelled.get()) {
-//                    System.err.println("isCanceled");
-//                    break;
-//                }
-//                try {
-//                    fluxSink.next("next " + i);
-//                } catch (RuntimeException e) {
-//                    System.err.println("error in sink: "+e.getMessage());
-//                    if (cancelled.get()) {
-//                        break;
-//                    }
-//                }
-//                try {
-//                    Thread.sleep(1000L);
-//                } catch (InterruptedException e) {
-//                    System.err.println("interrupted");
-//                    break;
-//                }
-//            }
-//
-//            System.err.println("complete2");
-//            fluxSink.complete();
-//        })
-//                .subscribeOn(Schedulers.single())
-//                .doOnSubscribe(d-> System.err.println("subscribe"))
-////                .onerror
-//                .subscribe(m->{
-//            try {
-//                if ("next 10".equals(m)) {
-//                    throw new RuntimeException("dasdsad");
-////                    emitter.completeWithError(new RuntimeException("dasdsad"));
-////                    emitter.send(SseEmitter.event().reconnectTime(0));
-////                    emitter.complete();
-////                    emitter.send(SseEmitter.event().reconnectTime(0));
-//                }
-//                emitter.send(m);
-//            } catch (IOException e) {
-//                System.err.println("IO exception");
-//                throw new RuntimeException(e);
-//            }
-//        }, ex -> {
-//            System.err.println("completeWithError");
-//            emitter.complete();
-////            emitter.completeWithError(ex);
-//
-////            throw new RuntimeException(ex);
-//        }, () -> {
-//            System.err.println("complete");
-//
-//            emitter.complete();
-//        });
-//
-//        emitter.onCompletion(() -> {
-//            System.err.println("onCompletion");
-////            s.dispose();
-//        });
-//        emitter.onTimeout(()-> System.err.println("onTimeerr"));
-//
-//        return emitter;
-//    }
-//
-
+		return emitter;
+	}
 
 }
 
--- src/test/java/com/example/demo/SseTest.java
+++ src/test/java/com/example/demo/SseTest.java
@@ -1,93 +1,81 @@
 package com.example.demo;
 
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
 import org.glassfish.jersey.media.sse.EventInput;
 import org.glassfish.jersey.media.sse.InboundEvent;
 import org.glassfish.jersey.media.sse.SseFeature;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-import sun.net.www.http.HttpClient;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import java.io.IOException;
-import java.text.MessageFormat;
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import static org.junit.Assert.assertEquals;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SseTest {
 
-    @LocalServerPort
-    private int port = 8080;
+	@LocalServerPort
+	private int port = 8080;
 
-    @Autowired
-    SseController sseController;
+	@Autowired
+	SseController sseController;
 
-    @Autowired
-    private TestRestTemplate restTemplate;
+	private static final Logger logger = LoggerFactory.getLogger(SseTest.class);
 
-    HttpClient client;
+	@Test
+	public void test() throws IOException, InterruptedException {
 
-    @Test
-    public void test() throws IOException, InterruptedException {
+		Client client = ClientBuilder.newBuilder().register(new SseFeature()).build();
+		WebTarget target = client.target("http://localhost:" + port);
+		EventInput e = null;
 
-        Client client = ClientBuilder.newBuilder().register(new SseFeature()).build();
-        WebTarget target = client.target("http://localhost:" + port + "/sse");
-        EventInput e = null;
+		AtomicInteger clientCallsNo = new AtomicInteger();
 
-        AtomicInteger clientCallsNo = new AtomicInteger();
+		Random r = new Random();
+		int low = 1;
+		int high = 5;
 
-        Random r = new Random();
-        int low = 1;
-        int high = 5;
+		for (int q = 0; q < 40; q++) {
+			int secs = r.nextInt(high - low) + low;
+			// we will close the connection after random number of seconds between 1 and 5
+			connectAndClose(target.path("/sse/" + clientCallsNo.incrementAndGet()), e, secs);
+		}
+	}
 
-        for (int q = 0; q < 40; q++) {
-            int secs = r.nextInt(high - low) + low;
-            // we will close the connection after random number of seconds between 1 and 5
-            connectAndClose(target.queryParam("callNo", clientCallsNo.get()), e, clientCallsNo, secs);
+	private void connectAndClose(WebTarget target, EventInput e, int secsBeforeClose) {
+		if (e == null || e.isClosed()) {
+			// (re)connect
+			logger.info("[CLIENT] connecting...");
+			e = target.request().get(EventInput.class);
+			e.setChunkType("text/event-stream");
+		}
 
-            for (int i = 0; i < 3; i++) {
-                Thread.sleep(1000);
+		for (int i = 0; i < secsBeforeClose; i++) {
+			final InboundEvent inboundEvent = e.read();
+			if (inboundEvent == null) {
+				logger.info("[CLIENT] server stream stopped");
+				break;
+			}
+			else {
+				String data = inboundEvent.readData();
+				logger.info("[CLIENT] client receives: " + data);
 
-                System.out.println(MessageFormat.format("server calls no: {0}, client calls no: {1}", sseController.callsNo, clientCallsNo.get()));
-                assertEquals(clientCallsNo.get(), sseController.callsNo);
-            }
-        }
-    }
-
-    private static void connectAndClose(WebTarget target, EventInput e, AtomicInteger clientCallsNo, int secsBeforeClose) {
-        for (int i = 0; i < secsBeforeClose; i++) {
-            if (e == null || e.isClosed()) {
-                // (re)connect
-                e = target.request().get(EventInput.class);
-
-                e.setChunkType("text/event-stream");
-                System.out.println("connecting...");
-                clientCallsNo.incrementAndGet();
-            }
-            final InboundEvent inboundEvent = e.read();
-            if (inboundEvent == null) {
-                System.out.println("server stream stopped");
-                break;
-            } else {
-                String data = inboundEvent.readData();
-                System.out.println(" client receives: " + data);
-
-            }
-
-        }
-
-        System.out.println("client closes connection");
-        e.close();
-    }
+			}
+		}
+		logger.info("[CLIENT] client closes connection");
+		e.close();
+	}
 }
